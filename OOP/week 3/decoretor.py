4094b22133d0fbade16be45acd45b3355a1aec50
farzanapomy <farzanapomy56@gmail.com>
2023-03-01 13:12:03 +0600

    dec-done

--- OOP/week 3/decoretor.py
+++ OOP/week 3/decoretor.py
@@ -1,15 +1,22 @@
+import math
+import time
+
+
 def timer(func):
-    def inner():
+    def inner(*args, **kwargs):
         print('Time start')
-        func()
-        print('Time end')
+        start = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        print(f'Time end.total time taken{end-start}')
     return inner
 
 
 @timer
-def get_factorial():
-    print('Factorial')
+def get_factorial(n):
+    result = math.factorial(n)
+    print(f'Factorial is {n} of {result}')
 
 
 # timer(get_factorial)()
-get_factorial()
+get_factorial(n=5)
