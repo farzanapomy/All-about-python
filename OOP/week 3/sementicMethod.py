84dad00743e9c7eeed759c18323e4e42bbe9e446
farzanapomy <farzanapomy56@gmail.com>
2023-04-03 00:56:11 +0600

    done

--- OOP/week 3/sementicMethod.py
+++ OOP/week 3/sementicMethod.py
@@ -8,7 +8,18 @@ class Shopping:
     def multiply(x, y):
         return x*y
 
+    def add_to_total(self, amount):
+        self.total += amount
+
     def add_to_cart(self, item, quantity, price):
         item_total = price*quantity
-        self.total += item_total
+        self.add_to_total(item_total)
         self.items.append(item)
+        self.customer = "customer"
+
+
+result = Shopping.multiply(2, 3)
+print(result)
+
+result2 = Shopping.add_to_total(2, 3)
+print(result2)
