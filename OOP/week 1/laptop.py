5842fa280af57736b7a51b5eafad580b75f3fea6
farzanapomy <farzanapomy56@gmail.com>
2023-04-22 00:52:31 +0600

    just-lame

--- OOP/week 1/laptop.py
+++ OOP/week 1/laptop.py
@@ -1,8 +1,9 @@
 class Laptop:
 
-    def __init__(self, brand, age):
+    def __init__(self, brand, age, price):
         self.brand = brand
         self.age = age
+        self.price = price
 
     def increase_age(self, age=1):
         self.age += age
@@ -12,10 +13,11 @@ class Laptop:
 
     def apply_discount(self, num):
         off_price = (self.price * num) / 100
-        return self.price - off_price
+        # return off_price
+        print(self.price - off_price)
 
 
-laptop1 = Laptop("hp", 17)
+laptop1 = Laptop("hp", 17, 1000)
 laptop1.increase_age()
 laptop1.increase_age()
 laptop1.increase_age()
@@ -23,5 +25,5 @@ print(laptop1.age)
 laptop1.repair()
 print(laptop1.age)
 print(laptop1.__dict__)
-# laptop1.apply_discount(10)
-# print(laptop1.price)
+laptop1.apply_discount(10)
+print(laptop1.price)
