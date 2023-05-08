1449acf702ba05b0de9eab751feebbcf13bbc56a
farzanapomy <farzanapomy56@gmail.com>
2023-05-09 01:10:57 +0600

    priv

---
+++ OOP/week 3/private.py
@@ -0,0 +1,25 @@
+class User:
+    def __init__(self, name, password, phone):
+        self.name = name
+        self.__password = password
+        self.phone = phone
+
+    # def get_password(self):
+    #     print(self.__password)
+
+    # private method
+    def __get_password(self):
+        print(self.__password)
+
+    def user_login(self, name, password):
+        if name == self.name and password == self.__password:
+            print("Login successful")
+        else:
+            print("Login failed")
+
+
+dal = User("Dal", "1234", "017000000")
+# print(dal.name)
+# print(dal.password)
+# dal.get_password()
+result = dal.user_login("Dal", "1234")
