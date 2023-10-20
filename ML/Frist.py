0afb161c7de9e2b9df17113b065ffa763a76c27c
farzanapomy <farzanapomy56@gmail.com>
2023-10-20 20:18:22 +0600

    practice

--- ML/Frist.py
+++ ML/Frist.py
@@ -1,5 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
-url='https://pypi.org/project/requests/'
-res=requests.get(url)
-print(res)
+url='https://en.wikipedia.org/wiki/IPhone'
+text=requests.get(url).text.encode('utf-8').decode('ascii', 'ignore')
+# print(text)
+soup= BeautifulSoup(text, 'lxml')
+table=soup.find('table', {'class':'wikitable'} )
+print((table))
+# rows=table.find_all('tr')
+# print(rows)
+
+# for row in rows:
+#     data=row.find_all(['th', 'td'])
+#     try:
+#      print(data[1:].a.text)
+#     except:
+#      pass
---
+++ ML/main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+data=pd.read_csv('ML\HR_comma_sep.csv')
+# print(data.shape)
+
+# find missing data
+# print(data.isnull().values.any())
+# print(data.dtypes)
+print(data.salary.unique(), data.sales.unique())
+# data.replace(to_replace='sales',value='1')
+
+clean_up_values={
+    "salary": {
+        'low':1,
+        'medium':2,
+        'high':3
+    },
+
+
+}
+data=data.replace(clean_up_values)
+# print(data)
+
+# get dummies for the departments
+dummies=pd.get_dummies(data.sales)
+# print(dummies)
+
+# merge dummies into original data
+
+merged=pd.concat([data,dummies],axis='columns')
+# print(merged)
+
+final_data=merged.drop(['sales'],axis=1)
+print(final_data)
